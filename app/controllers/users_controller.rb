5a0fd6a5771ad8d99afd769fb155e28b1faba1f9
iRa <ira.susanto@racerocks3d.com>
2025-02-25 11:12:44 -0800

    Add edit template and update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,14 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[ edit update ]
+
   def new
     @user = User.new
+  end
+
+  def show
+  end
+
+  def edit
   end
 
   def create
@@ -13,7 +21,20 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "Your account information was successfully updated"
+      redirect_to articles_path
+    else
+      render :edit
+    end
+  end
+
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.expect(user: [ :username, :email, :password ])
