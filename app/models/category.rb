068b5c66cd5209961bac4832f3ed591e1c376794
iRa <ira.susanto@racerocks3d.com>
2025-02-25 19:50:30 -0800

    Many-to-many association - back-end implementation

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,6 @@
 class Category < ApplicationRecord
   validates :name, presence: true, uniqueness: true, length: { in: 3..25 }
+
+  has_many :article_categories
+  has_many :articles, through: :article_categories
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_26_011544) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_26_033919) do
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
+
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
