13b45a24f78674ece8de1693209e7c9d96ce0d6e
iRa <ira.susanto@racerocks3d.com>
2025-02-26 14:57:24 -0800

    bin/rubocop -a

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -5,11 +5,11 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
 
   def sign_in_as(user, password: "password")
     visit login_path
-    
+
     fill_in "Email", with: user.email
     fill_in "Password", with: password
     click_button "Log in" # Adjust if using Devise (`Sign in`)
-    
+
     assert_text "Logged in successfully" # Optional assertion to confirm sign-in
   end
 end
